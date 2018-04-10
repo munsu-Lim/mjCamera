07c438aae8ac154bec81970c4cfb73d3af01ae12
mun-su <lms1427@gmail.com>
2018-04-10 19:15:20 +0900

    test_버튼리스너추가테스트

--- app/src/main/java/org/ajmediananumduo/mjcamera/MainActivity.java
+++ app/src/main/java/org/ajmediananumduo/mjcamera/MainActivity.java
@@ -2,6 +2,9 @@ package org.ajmediananumduo.mjcamera;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button testbutton = findViewById(R.id.button);
+        testbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"테스트메소드입니다",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
